2f4d603f6fec7d2dae6c5d7663eea9cb0c761ac0
ChaseBig <chase.small@outlook.com>
2020-08-12 15:42:31 -0500

    add basic test

---
+++ Test Suites/Login Tests/Basic Login Failure.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Basic Login Failure</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b531409f-9595-4f37-b463-4663fecb7772</testSuiteGuid>
+   <testCaseLink>
+      <guid>24e68a3a-bee3-4934-8bf7-c403d1839c83</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PC Browser Tests/PC Tests/Web Login Tests/Invalid username and password should return login error message</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
